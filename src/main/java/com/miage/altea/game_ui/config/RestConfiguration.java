4539ddc88f03f4f8037f8848ee324973dc8ff460
ramus <lukas.ramus.etu@univ-lille.fr>
2020-02-10 11:50:40 +0100

    Add : local translation

--- src/main/java/com/miage/altea/game_ui/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/game_ui/config/RestConfiguration.java
@@ -1,8 +1,13 @@
 package com.miage.altea.game_ui.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Configuration
 public class RestConfiguration {
@@ -12,4 +17,12 @@ public class RestConfiguration {
         return new RestTemplate();
     }
 
+
+    @Autowired
+    public void configureLocalInterceptor(RestTemplate restTemplate){
+        restTemplate.setInterceptors(List.of((httpRequest, bytes, clientHttpRequestExecution) -> {
+            httpRequest.getHeaders().add(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toLanguageTag());
+            return clientHttpRequestExecution.execute(httpRequest,bytes);
+        }));
+    }
 }
